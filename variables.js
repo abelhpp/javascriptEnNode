2a1d944e4a822adfd8200bb7e7a4e2b48617a0bb
abelhpp <abelipes@gmail.com>
2024-09-12 00:57:13 -0300

    servidorbasico

---
+++ variables.js
@@ -0,0 +1,8 @@
+let num1 = parseFloat(prompt("Ingresa el primer número:"));
+let num2 = parseFloat(prompt("Ingresa el segundo número:"));
+
+console.log("Suma:", num1 + num2);
+console.log("Resta:", num1 - num2);
+console.log("Multiplicación:", num1 * num2);
+console.log("División:", num1 / num2);
+console.log("Módulo:", num1 % num2);
